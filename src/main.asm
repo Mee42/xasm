a3e189add7addc34a1db69ac60029755e0deacf9
Carson Graham <carson42g@gmail.com>
2020-07-19 11:57:38 -0400

    REPL for cat program

--- src/main.asm
+++ src/main.asm
@@ -5,13 +5,23 @@ section .text
 
 
 _start:
-   
-   call read_line
-   mov ebx, eax
-   call print_string
-   call print_newline
-   mov ebx, 0 ; exit with code of 0
-   jmp exit
+    loop1: 
+      mov ebx, 0x24 ; $
+      call print_char
+      mov ebx, 32 ; ' '
+      call print_char
+      call read_line
+      mov bl, [eax]
+      cmp bl, 0
+      je start_exit
+      mov ebx, eax
+      call print_string
+      call print_newline
+    jmp loop1
+    
+    start_exit:
+      mov ebx, 0 ; exit with code of 0 
+      jmp exit
 
 ; takes in N, the number of bytes to malloc, in EBX
 ; returns a pointer to that memory in EAX
@@ -97,37 +107,6 @@ read_line:
     read_line_buffer_overflow:
       mov ebx, 2
       jmp exit
-
-
-      
-
-
-; takes in no arguments
-; returns a single character
-read_char:
-    push eax ; this is where we are going to store the character, so push garbage to hte }(=
-    mov ebx, 0 ; stdin
-    mov edx, 1 ; 1 byte
-    mov ecx, esp ; the buffer
-    mov eax, 0x3 ; sys_read
-    int 0x80
-    pop eax
-    cmp al, 0x0a
-    jne handleNewline
-    ret
-
-    handleNewline:
-    ; ok now we need to consume the \n
-    push eax
-    push eax ; scratch buffer space, again
-    mov ebx, 0
-    mov edx, 1
-    mov ecx, esp
-    mov eax, 0x3
-    int 0x80
-    pop eax
-    pop eax
-    ret
 
 
 print_newline:
